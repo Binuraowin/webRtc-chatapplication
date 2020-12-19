dd92ecd1163f7abb8b55f727a04f715d1236c3d0
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2020-12-20 01:20:13 +0530

    finished

--- chatapplication/index.js
+++ chatapplication/index.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const socket = require("socket.io");
+
 var app = express();
 
-app.listen(4000, function () {
+var server = app.listen(4000, function () {
     console.log("Listening to Port 4000");
 });
 
 app.use(express.static("public"));
+
+var upgradedServer = socket(server);
+
+upgradedServer.on("connection", function (socket) {
+    socket.on("sendingMessage", function (data) {
+        upgradedServer.emit("broadcastMessage", data);
+    });
+
+    console.log("Websocket Connected", socket.id);
+});
